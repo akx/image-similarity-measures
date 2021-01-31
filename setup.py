7f4f24cf061bc116c6c7b61a7360655520b1e0f0
Aarni Koskela <akx@iki.fi>
2021-01-31 19:27:06 +0200

    Make not-truly-necessary dependencies extras

--- setup.py
+++ setup.py
@@ -21,9 +21,10 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "Development Status :: 5 - Production/Stable"
     ],
-    install_requires=["numpy", "rasterio", "scikit-image", "opencv-python", "pyfftw", "phasepack"],
+    extras={"rasterio": ["rasterio"], "speedups": ["pyfftw"]},
+    install_requires=["numpy", "scikit-image", "opencv-python", "phasepack"],
     python_requires=">=3.6, <3.10",
     entry_points = {
         'console_scripts': ['image-similarity-measures=image_similarity_measures.evaluate:main'],
-    }
+    },
 )
